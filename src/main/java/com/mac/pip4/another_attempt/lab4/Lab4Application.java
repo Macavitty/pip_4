7dda80d6b557c24721adb7f388ccaf78bd2d5689
prilutskaya <prilutskaya_@ukr.net>
2019-03-14 05:20:44 +0300

    generating fat jar witn autoconfigs

--- src/main/java/com/mac/pip4/another_attempt/lab4/Lab4Application.java
+++ src/main/java/com/mac/pip4/another_attempt/lab4/Lab4Application.java
@@ -6,9 +6,9 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Lab4Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Lab4Application.class, args);
 	}
 
 }
+// Main-Class: com.mac.pip4.another_attempt.lab4.Lab4Application
